a5439d04f659c8371de033a927529a19f0031f58
zhanglong <zhanglong5288@126.com>
2016-12-06 23:54:38 +0800

    jdk自带的线程池

--- src/main/java/com/zl/thread_pool/concurrent02/MyRejected.java
+++ src/main/java/com/zl/thread_pool/concurrent02/MyRejected.java
@@ -11,6 +11,7 @@ public class MyRejected implements RejectedExecutionHandler{
 	
 	@Override
 	public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+		// 这里的r就是提交的任务
 		System.out.println("自定义处理..");
 		System.out.println("当前被拒绝任务为：" + r.toString());
 		
